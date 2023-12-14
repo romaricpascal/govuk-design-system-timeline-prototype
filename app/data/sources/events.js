c36a7bb4cd1dacc4fa17ac4fcec1c4a023da4e57
Romaric Pascal <romaric.pascal@digital.cabinet-office.gov.uk>
2023-12-14 11:47:00 +0000

    Render branches on the main timeline

--- app/data/sources/events.js
+++ app/data/sources/events.js
@@ -9,6 +9,7 @@ module.exports = {
       return {
         ...servicePhase,
         time: DATE_FORMAT.format(new Date(servicePhase.datetime)),
+        type: 'phase',
         classes: 'app-phase'
       }
     })
@@ -17,14 +18,33 @@ module.exports = {
       return {
         ...generalEvent,
         time: DATE_FORMAT.format(new Date(generalEvent.datetime)),
+        type: 'event',
         classes: 'app-event',
         url: generalEvent.events ? `/release?version=${generalEvent.id}` : `event?id=${generalEvent.id}`
       }
     })
 
-
     return {
       events: [...serviceEvents, ...generalEvents]
     }
   }
+}
+
+function getServicePhase(event, servicePhases) {
+  let servicePhase = {title: 'Discovery'};
+  // Iterate until we hit the service phase whose date is after this one
+  for(const currentServicePhase of servicePhases) {
+    if (currentServicePhase.datetime > event.datetime) {
+      return servicePhase;
+    }
+    servicePhase = currentServicePhase;
+  }
+
+  return servicePhase 
+}
+
+
+// Comparator for sorting an array by ascending order of the given property
+function ascending(propertyName) {
+  return (a,b) => a[propertyName] > b[propertyName] ? 1 : a[propertyName] < b[propertyName] ? -1 : 0
 }
